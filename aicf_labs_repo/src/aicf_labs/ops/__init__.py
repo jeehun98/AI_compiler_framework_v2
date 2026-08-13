3070fa2972193a76d83748e049014da18c84fb72
jeehun98 <64077080+jeehun98@users.noreply.github.com>
2026-08-13 21:04:51 +0900

    Export add operator

--- aicf_labs_repo/src/aicf_labs/ops/__init__.py
+++ aicf_labs_repo/src/aicf_labs/ops/__init__.py
@@ -1,6 +1,18 @@
+from .add import ADD, add
 from .bias_add import BIAS_ADD, bias_add
 from .gemm import GEMM, gemm
 from .relu import RELU, relu
 from .registry import all_definitions, get
 
-__all__ = ["BIAS_ADD", "GEMM", "RELU", "all_definitions", "bias_add", "gemm", "get", "relu"]
+__all__ = [
+    "ADD",
+    "BIAS_ADD",
+    "GEMM",
+    "RELU",
+    "add",
+    "all_definitions",
+    "bias_add",
+    "gemm",
+    "get",
+    "relu",
+]
